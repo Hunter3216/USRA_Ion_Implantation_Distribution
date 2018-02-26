0dc313e7adecf54a519caf53add0f2066aeca716
Hunter3216 <wck862@mail.usask.ca>
2018-02-26 15:56:14 -0600

    Revert "Added Comments to Data_Get and RangeStraggleInterpolator. Changed Data_gGet to return keV instead of eV. Set up RangeStraggleInterpolator to Interpolate Range and Straggle as a function of Energy using PCHIP."
    This reverts commit a868bae76b6cb4d1994c28b6c37d6356f851f617.

--- RangeStraggleInterpolator.m
+++ RangeStraggleInterpolator.m
@@ -1,15 +1,3 @@
-function [InterpRange, InterpStraggle] = RangeStraggleInterpolator(Energy, Range, Straggle)
-    %Purpose: Create Range and Straggle function of Voltage using
-    %   interpolation
-    %Pre-Conditions: All inputs aquired from Data_Get.m
-    %   Energy: Array of energies of ion distriubtions
-    %   Range: All of the mean ranges of distribution corresponding to Energy
-    %   Straggle: All of the standard-deviations of distributions corresponding to Energy.
-    %Return:
-    %   InterpRange: function of Range vs. Energy (i.e. Range(Energy))
-    %   InterpStraggle: function of Straggle vs. Energy (i.e.Straggle(Energy))
+function [InterpRange, InterpStraggle] = RangeStraggleInterpolator(Energies, Ranges, Straggle)
     
-    InterpRange = fit(Energy,Range,'pchip');
-    InterpStraggle = fit(Energy,Straggle,'pchip');
-    %pchip Piecewise Cubic Hermite Interpolating Polynomial (PCHIP)
 end
