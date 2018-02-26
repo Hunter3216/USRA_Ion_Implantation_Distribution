296f5f32bb4be4af5a13602d35e69b362c42d181
Hunter3216 <wck862@mail.usask.ca>
2018-02-26 15:58:09 -0600

    Revert "Revert "Added Comments to Data_Get and RangeStraggleInterpolator. Changed Data_gGet to return keV instead of eV. Set up RangeStraggleInterpolator to Interpolate Range and Straggle as a function of Energy using PCHIP.""
    This reverts commit 0dc313e7adecf54a519caf53add0f2066aeca716.

--- Data_Get.m
+++ Data_Get.m
@@ -1,4 +1,13 @@
-function [Voltages,Ranges,Straggle] = Data_Get(path)
+function [Energy,Range,Straggle] = Data_Get(path)
+    %Purpose: To read a SRIM text file (given it's path) and return the relevant
+    %   info (Energy, Range, Straggle)
+    %Pre-Conditions:
+    %   path: The path to text file
+    %Return:
+    %   Energy: all of the ion energies of distributions
+    %   Range: all of Energy's corresponding mean ranges
+    %   Straggle: all of Energy's corresponding Longitudinal Straggling (or standard deviations)
+
     %reads in file
     fileID = fopen(path,'r');
     s = textscan(fileID, '%s', 'delimiter', '\n');
@@ -16,14 +25,14 @@ function [Voltages,Ranges,Straggle] = Data_Get(path)
     end
     
     %Extracts these data from the relevant columns
-    Voltages = str2double(split(:,1));
-    Ranges = str2double(split(:,5));
+    Energy = str2double(split(:,1));
+    Range = str2double(split(:,5));
     Straggle = str2double(split(:,7));
     
-    %Corrects Voltages for when eV switches to keV
-    for k = 2 : length(C)
-       if Voltages(k) < Voltages(k-1) 
-           Voltages(k) = 1000*Voltages(k);
-       end
+    %Corrects Energy by switching eV to keV
+    k = 1;
+    while Energy(k+1) > Energy(k)
+        k = k+1;
     end
+    Energy(1:k) = Energy(1:k)./1000;
 end
